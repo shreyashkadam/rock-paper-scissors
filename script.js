4e8e03b0dcec59abd630fc062ad852d2fd0ab294
shreyashkadam <shreyash.kadam10@gmail.com>
2022-09-08 22:44:14 +0530

    Add function to get computer choice to script

---
+++ script.js
@@ -0,0 +1,11 @@
+const rspArr = ['Rock', 'Paper', 'Scissor'];
+
+function getComputerChoice(){
+    // algorithm to find random whole number 
+    // between any two whole numbers => return Math.floor(Math.random() * (max - min + 1)) + min
+    let randNum = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+    return rspArr[randNum - 1];    
+};
+
+let computerChoice = getComputerChoice();
+console.log(computerChoice);
